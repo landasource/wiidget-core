0615fb212e9b36cdaa36a40439e045dc994bd963
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2015-04-27 21:08:25 +0200

    Format code

--- src/main/java/com/landasource/wiidget/util/DefaultProperties.java
+++ src/main/java/com/landasource/wiidget/util/DefaultProperties.java
@@ -9,52 +9,52 @@ import java.util.Map;
  */
 public class DefaultProperties implements Properties {
 
-    private static Map<String, Object> DEFAULTS;
+	private static Map<String, Object> DEFAULTS;
 
-    static {
+	static {
 
-        final Map<String, Object> defaultConfig = new HashMap<String, Object>();
+		final Map<String, Object> defaultConfig = new HashMap<String, Object>();
 
-        defaultConfig.put(FORMAT_OUTPUT, false);
-        defaultConfig.put(FORMAT_INDENTATION, 4);
-        defaultConfig.put(WIIDGET_FILE_EXTENSION, ".wdgt");
+		defaultConfig.put(FORMAT_OUTPUT, false);
+		defaultConfig.put(FORMAT_INDENTATION, 4);
+		defaultConfig.put(WIIDGET_FILE_EXTENSION, ".wdgt");
 
-        DEFAULTS = Collections.unmodifiableMap(defaultConfig);
+		DEFAULTS = Collections.unmodifiableMap(defaultConfig);
 
-    }
+	}
 
-    public DefaultProperties() {
-        super();
+	public DefaultProperties() {
+		super();
 
-    }
+	}
 
-    @Override
-    public boolean getBoolean(final String key) {
+	@Override
+	public boolean getBoolean(final String key) {
 
-        return Boolean.parseBoolean(get(key).toString());
-    }
+		return Boolean.parseBoolean(get(key).toString());
+	}
 
-    @Override
-    public Object get(final String key) {
+	@Override
+	public Object get(final String key) {
 
-        return DEFAULTS.get(key);
-    }
+		return DEFAULTS.get(key);
+	}
 
-    @Override
-    public Integer getInteger(final String key) {
-        return Integer.parseInt(get(key).toString());
-    }
+	@Override
+	public Integer getInteger(final String key) {
+		return Integer.parseInt(get(key).toString());
+	}
 
-    @Override
-    public String getString(final String key) {
-        return String.valueOf(get(key));
-    }
+	@Override
+	public String getString(final String key) {
+		return String.valueOf(get(key));
+	}
 
-    @Override
-    public String getDefault(final String key, final String defaultValue) {
-        final Object value = get(key);
+	@Override
+	public String getDefault(final String key, final String defaultValue) {
+		final Object value = get(key);
 
-        return null == value ? defaultValue : String.valueOf(value);
-    }
+		return null == value ? defaultValue : String.valueOf(value);
+	}
 
 }
